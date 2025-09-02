632b4d0da848a03456238a8807f8f84022d0632d
FrankGrillo1991 <104669674+FrankGrillo1991@users.noreply.github.com>
2025-09-01 21:51:12 -0400

    Fix Wikipedia search JS: correct selectors, use title/snippet, real Wikipedia links

--- script.js
+++ script.js
@@ -1,42 +1,21 @@
 $(document).ready(function () {
     $(document).keypress(function (e) {
         if (e.which == 13) {
-            let webLink = "...api.php?action=query&list=search&srsearch=" + document.getElementById("search").value + "&utf8=&format=json";
+            let searchValue = document.getElementById("search").value;
+            let webLink = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + encodeURIComponent(searchValue) + "&utf8=&format=json&origin=*";
             $.ajax({
                 url: webLink,
-                dataType: "jsonp",
-                success: function (
-                    data
-                ) {
-                    $("div").remove(
-                        ".searchResult"
-                    );
-                    for (
-                        i = 0;
-                        i <
-                        data.query
-                        .search
-                        .length;
-                        i++
-                    ) {
-                        let titleForResult = 
-                        data.query
-                            .search[
-                                i
-                        ].snippet;
-                        $(
-                            "searchBox"
-                        ).append(
-                            '<a href="https://example.com/' + 
-                            titleForResult + 
-                            `" target="_blank"
-                            style="text-decoration:none">
-                            <div class="searchResult">
-                            <span class="searchTitle">` +
-                            titleForResult + 
-                            "</span><span><br />" +
-                            snippetForResult + 
-                            "</span></div></a>"
+                dataType: "json",
+                success: function (data) {
+                    $(".searchResult").remove();
+                    for (let i = 0; i < data.query.search.length; i++) {
+                        let title = data.query.search[i].title;
+                        let snippet = data.query.search[i].snippet;
+                        let pageUrl = "https://en.wikipedia.org/wiki/" + encodeURIComponent(title.replace(/ /g, "_"));
+                        $("#searchBox").append(
+                            '<a href="' + pageUrl + '" target="_blank" style="text-decoration:none">' +
+                            '<div class="searchResult">' +
+                            '<span class="searchTitle">' + title + '</span><span><br />' + snippet + '</span></div></a>'
                         );
                     }
                 },
